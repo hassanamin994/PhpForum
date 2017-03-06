696f3328223cba6925ea6b72efe5322e5c3f36b8
Seif El-Islam <seif.eleslam1990@gmail.com>
2017-03-06 06:46:03 +0200

    modify getCount to return number ratherthan array

--- model2/DBHandeller.php
+++ model2/DBHandeller.php
@@ -136,8 +136,9 @@ class DBHandeller {
         $query1 = "SELECT count($this->table.id) as NumOfChildren FROM  $this->table ";
         $prep1 = $this->db->prepare($query1);
         $prep1->execute();
-        $num = $prep1->fetchAll(PDO::FETCH_ASSOC);
-        return $num;
+        $result = $prep1->fetchColumn();
+        //$num=count($result);
+        return $result;
     }
     
 }
