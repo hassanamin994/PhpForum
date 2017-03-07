0e56cc95fe555b15307f71d54ef2eaefefac4be7
salmabadr <salmabadr00@gmail.com>
2017-03-07 13:10:54 +0200

    viemore page

---
+++ viewmore.php
@@ -0,0 +1,18 @@
+<?php include('header.php') ; ?>
+<html>
+
+<head>
+<link href='http://fonts.googleapis.com/css?family=Lobster&subset=all' rel='stylesheet' type='text/css'>
+                <link href="assets/css/bootstrap.min.css" rel="stylesheet">
+                <link href="assets/css/style.css" rel="stylesheet">
+</head>
+<body class="back">
+<form>
+<div class="container cont" >
+<div style="float: right">  <a href="" class="btn btn-default glyphicon glyphicon-edit">Edit</a>
+<a href="" class="btn btn-default glyphicon glyphicon-pencil">Reply</a>
+</div>
+</div>
+</form>
+</body>
+</html>
